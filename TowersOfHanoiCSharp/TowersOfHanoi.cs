af0c78a9f0418696fb3d5d9d56ed0c2e804f99a9
ali tamim <amoh331@gmail.com>
2022-09-02 22:55:20 +0300

    update MoveDisk

--- TowersOfHanoiCSharp/TowersOfHanoi.cs
+++ TowersOfHanoiCSharp/TowersOfHanoi.cs
@@ -1,16 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace TowersOfHanoiCSharp
 {
     static class TowersOfHanoi
     {
-        public static void MoveDisk()
+        public static void MoveDisk(int n, char fromPeg, char toPeg, char auxPeg)
         {
+            // if 1 desk, make the move and return
+            if (n == 1)
+            {
+                Console.WriteLine($"Move desk 1 fron peg {fromPeg} to peg {toPeg}");
+                return;
+            }
 
+            // move top n-1 desks from A To B usin C as auxitiary
+            MoveDisk(n-1, fromPeg, auxPeg, toPeg);
+            Console.WriteLine($"Move desk {n} fron peg {fromPeg} to peg {toPeg}");
+
+            // move top n-1 desks from B To C usin A as auxitiary
+            MoveDisk(n - 1, auxPeg, toPeg, fromPeg);
         }
     }
 }
